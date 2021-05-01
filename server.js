17c60fbd6a7162d2226062615d6ba976625f274f
Sarang Vasishth <sarang.vasishth@gmail.com>
2021-05-01 15:38:02 +0530

    api security packages

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
 require("colors");
 require("dotenv").config();
 
+const hpp = require("hpp");
 const express = require("express");
+const xss = require("xss-clean");
 const logger = require("morgan");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
 
 const connectDB = require("./config/db");
 const errorHandler = require("./middlewares/error");
-const ErrorResponse = require("./utils/errorResponse");
 
 const { DEVELOPMENT } = require("./src/constants");
 
 connectDB();
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+
+// api security measures
+app.use(hpp());
+app.use(xss());
+app.use(helmet());
+app.use(limiter());
+app.use(mongoSanitize());
 
 // dev logging middleware
 if (process.env.NODE_ENV === DEVELOPMENT) {
